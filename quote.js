14a592bbf6a32c0b771323ebb7b9ac60de70abcd
Aric <aessig32@hotmail.com>
2018-02-28 08:20:15 -0600

    added twitter widget

--- quote.js
+++ quote.js
@@ -3,22 +3,12 @@ $(document).ready(function(){
 		e.preventDefault();
 		var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
 		$.getJSON(url, function(data){
-  			$('<h2 class="mx-5 mt-5 mb-2 text-center"><q>' + data.quoteText + '</q></h2>')
-  					.insertAfter("h1")
-  					.after('<p class="mb-5 text-right pr-5 mr-5"><i>' + data.quoteAuthor + '</i></p>')
-});
-		// $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1?jsoncallback=?", function(a) {
-		//   $("div").append(a[0].content + "<p>— " + a[0].title + "</p>")
-		// })
-
-		// $.ajax({
-		// url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",	
-		// success: function(data) {
-		// 	var post = data.shift();
-		// 	$(".quoteArea").html(post.content);
-		// }, 
-		// cache: false
-		// })
+  			$("h3").html('<q>' + data.quoteText + '</q>');
+			$("#author").text(data.quoteAuthor);
+		$("#tweet").on("click", function(){
+			$(this).attr("href", 'https://twitter.com/intent/tweet?text=');
+		});
+		})
 	})
 	
 })
